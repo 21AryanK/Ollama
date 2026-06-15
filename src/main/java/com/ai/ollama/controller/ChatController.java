99085f1b1058379df06e526b209be890f883f4ab
21AryanK <2101020528@cgu-odisha.ac.in>
2026-06-16 01:07:02 +0530

    Adding flux functionality

--- src/main/java/com/ai/ollama/controller/ChatController.java
+++ src/main/java/com/ai/ollama/controller/ChatController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 @RequestMapping("/api")
@@ -18,5 +19,10 @@ public class ChatController {
     @GetMapping("/chat")
     public String chat(@RequestParam("message") String message){
         return chatClient.prompt().user(message).call().content();
+    }
+
+    @GetMapping("/stream")
+    public Flux<String> streaming(@RequestParam("message") String message){
+        return chatClient.prompt().user(message).stream().content();
     }
 }
